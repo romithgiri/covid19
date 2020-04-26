719921b5f74c2ab09437aab7cb64cb4f25893e77
romithgiri <romithgiri@gmail.com>
2020-04-26 23:26:00 +0530

    Long list view with search functionality

--- lib/countryList.dart
+++ lib/countryList.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
@@ -11,69 +12,76 @@ class CountryList extends StatefulWidget {
   _CountryListPageState createState() => _CountryListPageState();
 }
 
+class CountryDataCls {
+  final String country;
+  final String countryCode;
+  final String slug;
+  final String newConfirmed;
+  final String totalConfirmed;
+  final String newDeaths;
+  final String totalDeaths;
+  final String newRecovered;
+  final String totalRecovered;
+  final String date;
+
+  CountryDataCls({
+    this.country,
+    this.countryCode,
+    this.slug,
+    this.newConfirmed,
+    this.totalConfirmed,
+    this.newDeaths,
+    this.totalDeaths,
+    this.newRecovered,
+    this.totalRecovered,
+    this.date,
+  });
+
+  factory CountryDataCls.fromJson(Map<String, dynamic> json) {
+    return new CountryDataCls(
+      country: json['Country'].toString(),
+      countryCode: json['CountryCode'].toString(),
+      slug: json['Slug'].toString(),
+      newConfirmed: json['NewConfirmed'].toString(),
+      totalConfirmed: json['TotalConfirmed'].toString(),
+      newDeaths: json['NewDeaths'].toString(),
+      totalDeaths: json['TotalDeaths'].toString(),
+      newRecovered: json['NewRecovered'].toString(),
+      totalRecovered: json['TotalRecovered'].toString(),
+      date: json['Date'].toString(),
+    );
+  }
+}
+
 class _CountryListPageState extends State<CountryList> {
   TextEditingController _textController = TextEditingController();
-
   bool toggle = false;
-  var data;
+  var data = [];
   var dataCount;
+  List<CountryDataCls> dataFromServer = List();
+  List<CountryDataCls> filterData = List();
 
   @override
   void initState() {
     super.initState();
-    asyncOperation().then((val) {}).catchError((error, stackTrace) {
-      print("C33333333333333: $error");
-    });
+    asyncOperation().then((val) {}).catchError((error, stackTrace) {});
   }
 
-  Future<void> asyncOperation() async {
-    var url = 'https://api.covid19api.com/summary';
-    var response = await http.get(url);
-    if (response.statusCode == 200) {
-      var jsonResponse = convert.jsonDecode(response.body);
-      setState(() {
-        data = jsonResponse['Countries'];
-        dataCount = data.length;
-      });
-      await toggleLoader();
-      return 0;
-    } else {
-      print('Error : ${response.statusCode}.');
-      return 0;
-    }
-  }
-
-  void filterSearchResults(String query) {
-    print('Test ------------------------------- 111');
-  } //Now using
-
-  onItemChanged(String value) {
-    print('######################################################################');
-    var oldData = data;
-    var n = data.length;
-    print('1 ------: $oldData');
-    print('2 ------: $n');
-    data = null;
-    print('3 ------: $data');
+  Future<List<CountryDataCls>> asyncOperation() async {
+    http.Response response = await http.get('https://api.covid19api.com/summary');
+    var responseJson = json.decode(response.body);
     setState(() {
-      for (var i = 0; i < n; i++) {
-        print('Inside for loop :- $i');
-        print('********* :- $value');
-        print('__________________________________________________');
-        if (oldData[i]['Country'].where((string) =>
-            string.toLowerCase().contains(value.toLowerCase())).toList()) {
-          data.add(oldData[i]);
-        }
-      }
+      dataFromServer = (responseJson['Countries'] as List).map((p) => CountryDataCls.fromJson(p)).toList();
+      filterData = dataFromServer;
     });
-    print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+    await toggleLoader();
+    return  dataFromServer = (responseJson['Countries'] as List).map((p) => CountryDataCls.fromJson(p)).toList();
   }
 
   Widget getListView() {
-//    var listItems = getListElements();
-    var listItems = data;
+    var listItems = filterData;
     var listview = ListView.builder(
-        itemCount: data.length,
+        itemCount: filterData.length,
         shrinkWrap: true,
         itemBuilder: (context, index) {
           return Container(
@@ -87,9 +95,9 @@ class _CountryListPageState extends State<CountryList> {
                   color: Colors.white,
                   elevation: 10,
                   child: Padding(
-                    padding: EdgeInsets.all(10.00),
+                      padding: EdgeInsets.all(10.00),
                       child: Text(
-                        listItems[index]['Country'],
+                        listItems[index].country,
                         style: TextStyle(
                             color: Colors.pinkAccent[700],
                             fontSize: 20.00,
@@ -109,33 +117,39 @@ class _CountryListPageState extends State<CountryList> {
         body: Container(
             color: Colors.pinkAccent[700],
             child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: TextField(
-                    controller: _textController,
-                    decoration: InputDecoration(
-                      hintText: 'Search Here...',
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: TextField(
+                      controller: _textController,
+                      decoration: InputDecoration(
+                        hintText: 'Search Here...',
+                      ),
+                      onChanged: (value){
+                        setState((){
+                          filterData = dataFromServer
+                              .where((u) => (u.country
+                              .toLowerCase()
+                              .contains(value.toLowerCase())
+                          )).toList();
+                        });
+                      },
                     ),
-                    onChanged: (value){
-                      filterSearchResults(value);
-                    },
                   ),
-                ),
-                Expanded(
-                  child:
-                    toggle ? getListView()
-                        : Column(children: <Widget>[
-                      Center(
-                        child: Loading(
-                          indicator: BallPulseIndicator(),
-                          size: 100.0,
-                          color: Colors.white,
-                        ),
-                      )
-                    ])
-                )
-        ])
+                  Expanded(
+                      child:
+                      toggle ? getListView()
+                          : Column(children: <Widget>[
+                        Center(
+                          child: Loading(
+                            indicator: BallPulseIndicator(),
+                            size: 100.0,
+                            color: Colors.white,
+                          ),
+                        )
+                      ])
+                  )
+                ])
         )
     );
   }
